6060c0ecb46cfe151645017fdf42043a300a3bf8
medusa <142658222+7medusa@users.noreply.github.com>
2023-12-26 12:02:59 +0100

    Update hash.py
    updating the comments

--- hash.py
+++ hash.py
@@ -2,8 +2,8 @@ import hashlib
 import binascii
 
 def hash_berechnung(content):
-    data = content.strip()  # Entferne Leerzeichen am Anfang und Ende der Zeichenkette
-    data_bytes = data.encode('utf-8')#codierung in bytes
-    hash_object = hashlib.sha256()#festelgen der hash codierung
-    hash_object.update(data_bytes)#codierung der eingabe
-    return hash_object.hexdigest()#codierung returnen in hex
+    data = content.strip()  # deleting space for the correct hash
+    data_bytes = data.encode('utf-8')
+    hash_object = hashlib.sha256() #  sha256
+    hash_object.update(data_bytes)
+    return hash_object.hexdigest()
