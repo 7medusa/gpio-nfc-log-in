37503522912d692f8726180e540c1c9a88dee6d3
medusa <142658222+7medusa@users.noreply.github.com>
2023-12-27 01:33:34 +0100

    Update main.py
    i added an option to log in with an nfc tag

--- main.py
+++ main.py
@@ -1,8 +1,14 @@
-import RPi.GPIO as GPIO
 from mfrc522 import SimpleMFRC522
-import hash
+import RPi.GPIO as GPIO
 from time import sleep
+import subprocess
+import hash
 
+hash_code = "b0c3fd36b5de1487ecf726db8edc1c05ab8b97f0ee224f9842b27d69844c2f51" #  hash of the password on the nfc_tag
+username = "username" #  username, please change this
+passwort = "password" #  in work...
+
+ #  light control beginning
 GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BCM)
 green = 21
@@ -13,27 +19,28 @@ def led_on(color):
     GPIO.output(color, GPIO.HIGH)
 def led_off(color):
     GPIO.output(color, GPIO.LOW)
+ #  light control ending
 
 
 def read_nfc():
-    global text
+    global nfc_text
     reader = SimpleMFRC522()
-    print("Bitte halte eine NFC-Karte vor den Reader...")
-    id, text = reader.read()
+    print("waiting...")
+    id, nfc_text = reader.read()
 
-def write_nfc(data):
-    reader = SimpleMFRC522()
-    print("Bitte halte eine leere NFC-Karte vor den Reader...")
-    print("Schreibe Daten auf die Karte:", data)
-    reader.write(data)
-    print("Daten erfolgreich geschrieben.")
+
+def log_in(username, password):
+    command = f"echo '{password}' | sudo -S -u {username} bash -c 'echo Login successful'"
+    result = subprocess.run(command, shell=True, check=True, text=True, input=password)
+
 
 if __name__ == "__main__":
     while True:
         read_nfc()
-        if str(hash.hash_berechnung(text)) == "b0c3fd36b5de1487ecf726db8edc1c05ab8b97f0ee224f9842b27d69844c2f51":
+        if str(hash.hash_berechnung(nfc_text)) == hash_code:
             print("korrekt")
             led_on(green)
+            log_in(username, passwort)
             sleep(5)
             led_off(green)
         else:
