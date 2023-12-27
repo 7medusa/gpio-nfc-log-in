5dfae15cde541985d7e74c3904838ea4a6b53367
medusa <142658222+7medusa@users.noreply.github.com>
2023-12-27 17:24:08 +0100

    Delete hash.py

--- hash.py
+++
@@ -1,9 +0,0 @@
-import hashlib
-import binascii
-
-def hash_berechnung(content):
-    data = content.strip()  # deleting space for the correct hash
-    data_bytes = data.encode('utf-8')
-    hash_object = hashlib.sha256() #  sha256
-    hash_object.update(data_bytes)
-    return hash_object.hexdigest()
