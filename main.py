b111839511ba51cd5b97e353302bb843aefd54c9
medusa <142658222+7medusa@users.noreply.github.com>
2023-12-27 17:23:37 +0100

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from mfrc522 import SimpleMFRC522
 import RPi.GPIO as GPIO
 from time import sleep
 import subprocess
-import hash
+import hashlib
 
 hash_code = "14b114aa5fadf41b4caa65f2dd788c0ae04a543231a6fadc7435257299dcec0d" #  hash of the password on the nfc_tag
 
@@ -27,6 +27,14 @@ def read_nfc():
     id, nfc_text = reader.read()
 
 
+def hash_berechnung(content):
+    data = content.strip()
+    data_bytes = data.encode('utf-8')
+    hash_object = hashlib.sha256()
+    hash_object.update(data_bytes)
+    return hash_object.hexdigest()
+
+
 def log_in():
     command = "startx"
     subprocess.run(command, shell=True, capture_output=True)
@@ -35,7 +43,7 @@ def log_in():
 if __name__ == "__main__":
     while True:
         read_nfc()
-        if str(hash.hash_berechnung(nfc_text)) == hash_code:
+        if str(hash_berechnung(nfc_text)) == hash_code:
             print("correct")
             led_on(green)
             log_in()
